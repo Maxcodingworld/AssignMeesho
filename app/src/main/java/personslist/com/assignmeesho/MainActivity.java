2e1c45cc0d6103d2078bbf58dec4c64787fb0919
Sanyam Jain <Max@Sanyams-MacBook-Pro.local>
2017-10-08 13:53:13 +0530

    parsing JSON

--- app/src/main/java/personslist/com/assignmeesho/MainActivity.java
+++ app/src/main/java/personslist/com/assignmeesho/MainActivity.java
@@ -6,16 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import personslist.com.assignmeesho.pojo.PullRequestitem;
 
 public class MainActivity extends AppCompatActivity{
 
     Context context = null;
+
+    ArrayList<PullRequestitem> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +36,110 @@ public class MainActivity extends AppCompatActivity{
         new APIAsyncTask(context, new FinishAPI() {
             @Override
             public void onFinish(String result) {
-
+                JSONArray array = null;
+                try {
+                    array = new JSONArray(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                parseJsonArray(array);
             }
         }).execute();
 
     }
 
+    private void parseJsonArray(JSONArray array){
+        if(items!=null){
+            items.clear();
+        }
+        if(array == null || array.length()==0){
+            return;
+        }
 
+        for(int i=0;i<array.length();i++){
+            JSONObject object = null;
+            try {
+                object = (JSONObject) array.get(i);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if(object!=null){
+                PullRequestitem item = new PullRequestitem();
+                try {
+                    item.setId(object.getLong("id"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    item.setId(null);
+                }
+
+                try {
+                    item.setNumber(object.getInt("number"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setState(object.getString("state"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setTitle(object.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setUrl(object.getString("url"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    item.setUserName(object.getJSONObject("user").getString("login"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setAvatorUrl(object.getJSONObject("user").getString("avatar_url"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setSiteAdmin(object.getJSONObject("user").getBoolean("site_admin"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setUpdatedAt(object.getString("updated_at"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    item.setRepoName(object.getJSONObject("head").getJSONObject("repo").getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if(items!=null){
+                    items.add(item);
+                }
+            }
+        }
+    }
 
     public class APIAsyncTask extends AsyncTask<Boolean, Void, Boolean> {
         private String TAG = APIAsyncTask.class.getName();
         Context mContext;
         String result = "";
         FinishAPI callback = null;
-        String owner = "Maxcodingworld";  
+        String owner = "Maxcodingworld";
         String repo = "AssignMeesho";
 
         public APIAsyncTask(Context context,FinishAPI callback) {
---
+++ app/src/main/java/personslist/com/assignmeesho/pojo/PullRequestitem.java
@@ -0,0 +1,104 @@
+package personslist.com.assignmeesho.pojo;
+
+/**
+ * Created by Max on 08/10/17.
+ */
+
+public class PullRequestitem {
+
+    Long id;
+    String url;
+    Integer number;
+    String state;
+    String title;
+
+    // /user/login
+    String userName = null;
+    String avatorUrl = null;
+    Boolean siteAdmin = false;
+
+    String updatedAt = null;
+
+    //head/repo
+    String repoName = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAvatorUrl() {
+        return avatorUrl;
+    }
+
+    public void setAvatorUrl(String avatorUrl) {
+        this.avatorUrl = avatorUrl;
+    }
+
+    public Boolean getSiteAdmin() {
+        return siteAdmin;
+    }
+
+    public void setSiteAdmin(Boolean siteAdmin) {
+        this.siteAdmin = siteAdmin;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
